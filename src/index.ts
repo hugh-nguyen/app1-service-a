804ccecd8e0c7d917ddbda1afe683ad4beda36ec
Hugh-Nguyen1_iag <Hugh.Nguyen1@iag.com.au>
2025-03-09 11:14:27 +1100

    update for eks

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-// SERVICE-A
 import express, { Request, Response } from 'express';
 import axios from 'axios';
 import { instrumentWithAxon } from 'cortex-axon-js';
@@ -7,10 +6,7 @@ const app = express();
 
 app.use(instrumentWithAxon);
 
-axios.defaults.proxy = {
-  host: 'envoy',
-  port: 8080
-};
+axios.defaults.baseURL = 'http://envoy:8080';
 
 app.get('/a/getresult', async (req: Request, res: Response) => {
   const x = parseInt(req.query.x as string, 10);
@@ -20,7 +16,7 @@ app.get('/a/getresult', async (req: Request, res: Response) => {
   }
 
   try {
-    const response = await axios.get(`http://localhost/b/getresult?x=${x}&y=${y}`);
+    const response = await axios.get(`/b/getresult?x=${x}&y=${y}`);
     return res.status(response.status).json(response.data);
   } catch (err: any) {
     console.error("Error calling /b/getresult:", err.message);
