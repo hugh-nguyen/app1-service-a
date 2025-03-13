25cd8c71fd19b87a8781f09ec3f52eb290cc1ef3
Hugh-Nguyen1_iag <Hugh.Nguyen1@iag.com.au>
2025-03-13 23:24:01 +1100

    Nexus updates

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,27 @@
+// app1-service-a
 import express, { Request, Response } from 'express';
 import axios from 'axios';
 import { instrumentWithAxon } from 'cortex-axon-js';
 
 const app = express();
 
-app.use(instrumentWithAxon);
+app.use(instrumentWithAxon("app1"));
 
 axios.defaults.proxy = {
   host: 'envoy',
   port: 8080
 };
 
-app.get('/a/getresult', async (req: Request, res: Response) => {
-  const x = parseInt(req.query.x as string, 10);
-  const y = parseInt(req.query.y as string, 10);
-  if (Number.isNaN(x) || Number.isNaN(y)) {
-    return res.status(400).json({ error: 'Missing x or y' });
-  }
-
+app.get('/getresult', async (req: Request, res: Response) => {
   try {
-    const response = await axios.get(`http://service-b/b/getresult?x=${x}&y=${y}`);
-    return res.status(response.status).json(response.data);
+    const response = await axios.get(`http://localhost/app1/service-b/getresult/`);
+    return res.status(response.status).json(["A=33", ...response.data]);
   } catch (err: any) {
     console.error("Error calling /b/getresult:", err.message);
     return res.status(500).json({ error: "Something went wrong" });
   }
 });
 
-app.listen(80, () => {
-  console.log('service-a listening on port 80');
-});
-
-app.get('/health', (req, res) => {
-  res.status(200).send('OK');
+app.listen(5030, () => {
+  console.log('service-a listening on port 5030');
 });
